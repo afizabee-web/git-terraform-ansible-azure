d96ddfa21dd429ec37e6d1b7f058c16a1ea812cb
Ubuntu <azureuser@VM-A.zh1ywasisvke1lrtvf32cld0bf.rx.internal.cloudapp.net>
2025-12-19 18:55:00 +0000

    updated terraform file for updated inventory

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,89 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
+
+# -----------------------------
+# Resource Group
+# -----------------------------
 resource "azurerm_resource_group" "rg" {
-  name     = "git-tf-ansible-rg"
+  name     = "rg-devops-demo"
   location = "Central India"
 }
 
-resource "azurerm_public_ip" "pip" {
-  name                = "git-tf-ansible-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-}
-
+# -----------------------------
+# Virtual Network & Subnet
+# -----------------------------
 resource "azurerm_virtual_network" "vnet" {
-  name                = "git-tf-vnet"
-  address_space       = ["10.2.0.0/16"]
+  name                = "vnet-devops"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "git-tf-subnet"
+  name                 = "subnet-devops"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.2.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
+# -----------------------------
+# Network Security Group
+# -----------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-devops"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# -----------------------------
+# Public IP
+# -----------------------------
+resource "azurerm_public_ip" "public_ip" {
+  name                = "pip-devops"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+# -----------------------------
+# Network Interface
+# -----------------------------
 resource "azurerm_network_interface" "nic" {
-  name                = "git-tf-nic"
+  name                = "nic-devops"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -36,17 +91,24 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# -----------------------------
+# Linux Virtual Machine
+# -----------------------------
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "git-tf-ansible-vm"
+  name                = "vm-devops"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
-
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
@@ -54,7 +116,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_ssh_key {
     username   = "azureuser"
     public_key = file("${path.module}/ssh_key.pub")
-
   }
 
   os_disk {
@@ -68,5 +129,16 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+}
+
+# -----------------------------
+# Auto-generate Ansible Inventory
+# -----------------------------
+resource "local_file" "ansible_inventory" {
+  filename = "${path.module}/../ansible/inventory.ini"
+  content  = <<EOF
+[web]
+vmweb ansible_host=${azurerm_public_ip.public_ip.ip_address} ansible_user=azureuser
+EOF
 }
 
